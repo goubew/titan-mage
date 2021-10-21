bc9ed2c2a97967d3ce90642d819251d8273a6bfc
goubew <goubew@users.noreply.github.com>
2021-10-20 20:54:33 -0400

    Working example test case

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const ajv = require("ajv");
+const fs = require("fs");
 const yaml = require("yaml");
 
-const schemas = ["armor", "foes", "items", "spells", "weapons"]
+//const resourceTypes = ["armor", "foes", "items", "spells", "weapons"]
+const referenceTypes = ["armor"]
+let references = {}
 
-module.exports = { schemas };
+// Parse the yaml files into json
+function loadReferences() {
+  for (const reference of referenceTypes) {
+    const referenceData = fs.readFileSync(`./references/${reference}.yaml`, {encoding: "utf8"});
+    references[reference] = yaml.parse(referenceData);
+  }
+}
+
+module.exports = { references, referenceTypes, loadReferences };
 
 if (require.main === module) {
   //Do the stuff
--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 const assert = require("assert");
-const { schemas } = require("../index.js");
+const fs = require("fs");
+const Ajv = require("ajv");
+const ajv = new Ajv();
 
-describe("References match the schemas", function() {
-  for (const schema in schemas) {
-    it(`${schema} matches its schema`, function() {
+let { references, referenceTypes, loadReferences } = require("../index.js");
+
+describe("References match the schemas", () => {
+  it("Can convert the reference files from yaml to json", () => { loadReferences() });
+  for (const reference of referenceTypes) {
+    it(`${reference} matches its schema`, () => {
+      const referenceData = references[reference];
+      assert(referenceData, JSON.stringify(referenceData));
+
+      const schema = JSON.parse(fs.readFileSync(`./references/${reference}-schema.json`, {encoding: "utf8"}));
+      const validate = ajv.compile(schema);
+      const validateResult = validate(references[reference]);
+      assert(validateResult, JSON.stringify(validate.errors));
     });
   }
 });
