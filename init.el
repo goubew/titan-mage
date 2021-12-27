927159f3ea6edaea9dbb7da86288b5216a79d6f0
goubew <goubew@users.noreply.github.com>
2021-12-27 16:29:43 -0500

    Remove postamble and index toc

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
+      org-html-postamble nil                  ;; Remove the emacs and org version from the end of files
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; Define the publishing project
